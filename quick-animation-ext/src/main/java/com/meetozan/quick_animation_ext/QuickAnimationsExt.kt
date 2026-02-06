17b75b42154373e0ac1eed42305fa1681fd62060
meetOzan <mertozankahraman@gmail.com>
2026-02-07 02:50:12 +0300

    Apache license command line added

--- quick-animation-ext/src/main/java/com/meetozan/quick_animation_ext/QuickAnimationsExt.kt
+++ quick-animation-ext/src/main/java/com/meetozan/quick_animation_ext/QuickAnimationsExt.kt
@@ -1,8 +1,21 @@
-package com.meetozan.quick_animation_ext
-
 /*
-   Created by meetOzan on 04.02.2026
-*/
+ * Copyright 2026 The Android Open Source Project
+ * Created by meetOzan on 04.02.2026
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.meetozan.quick_animation_ext
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.CubicBezierEasing
@@ -22,7 +35,6 @@ import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import kotlinx.coroutines.delay
-
 
 /**
  * A modifier that adds a bounce effect when the composable is clicked.
